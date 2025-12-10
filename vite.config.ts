ce42288c37dee459df8f965214b4ee9fa1fbc52b
阿棒 <u9923015@gmail.com>
2025-12-10 10:53:39 +0800

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // 設定 base 為 './' 確保在 GitHub Pages 子目錄也能正常運作
-  base: './', 
-})
